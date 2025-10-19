4935a3d3b7bb8420a705ad3651bb90c57e25958c
HariomArka <arkaghosh9832@gmail.com>
2025-10-19 19:58:23 +0530

    Arka

--- routes/mail.js
+++ routes/mail.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { sendMail } = require('../Controllers/mailController');
+const { sendMail } = require('../controllers/mailController');
 
 router.post('/contact', sendMail);
 
